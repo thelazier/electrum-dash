0349a6f03d430f869a841203f717b70aea196d8a
TheLazieR Yip <thelazier@gmail.com>
2017-03-20 06:15:45 -0400

    No need brew install patch

--- .travis_install.sh
+++ .travis_install.sh
@@ -5,7 +5,7 @@ save_cd=`pwd`
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 # Install some custom requirements on OS X
-brew install protobuf zbar upx patch
+brew install protobuf zbar upx
 # VirtualEnv
 if source $HOME/virtualenv/bin/activate; then 
   python --version
