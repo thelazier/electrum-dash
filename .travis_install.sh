d50052c95f33f24c67a4b948878bcdf0622c3320
TheLazieR Yip <thelazier@gmail.com>
2017-03-20 05:15:41 -0400

    Add patch for Qt4.8.7

--- .travis_install.sh
+++ .travis_install.sh
@@ -5,7 +5,7 @@ save_cd=`pwd`
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 # Install some custom requirements on OS X
-brew install protobuf zbar upx
+brew install protobuf zbar upx patch
 # VirtualEnv
 if source $HOME/virtualenv/bin/activate; then 
   python --version
@@ -28,6 +28,27 @@ wget https://download.qt.io/official_releases/qt/4.8/4.8.7/qt-everywhere-opensou
 gunzip qt-everywhere-opensource-src-4.8.7.tar.gz
 tar -xf qt-everywhere-opensource-src-4.8.7.tar
 cd qt-everywhere-opensource-src-4.8.7
+cat > patchset1.patch << EOF
+index 4aa0668..63b646d 100644 (file)
+--- a/src/gui/painting/qpaintengine_mac.cpp
++++ b/src/gui/painting/qpaintengine_mac.cpp
+@@ -340,13 +340,7 @@ CGColorSpaceRef QCoreGraphicsPaintEngine::macDisplayColorSpace(const QWidget *wi
+     }
+ 
+     // Get the color space from the display profile.
+-    CGColorSpaceRef colorSpace = 0;
+-    CMProfileRef displayProfile = 0;
+-    CMError err = CMGetProfileByAVID((CMDisplayIDType)displayID, &displayProfile);
+-    if (err == noErr) {
+-        colorSpace = CGColorSpaceCreateWithPlatformColorSpace(displayProfile);
+-        CMCloseProfile(displayProfile);
+-    }
++    CGColorSpaceRef colorSpace = CGDisplayCopyColorSpace(displayID);
+ 
+     // Fallback: use generic DeviceRGB
+     if (colorSpace == 0)
+EOF
+patch -p1 <patchset1.patch
 ./configure -prefix $HOME/virtualenv -release -opensource -confirm-license -nomake examples -nomake tests -nomake demos -no-cups -no-sql-psql -no-sql-sqlite -qt-zlib -qt-libjpeg -qt-libpng -qt-libmng -qt-libtiff -no-qt3support -no-webkit 
 make -j4 2>&1 >> /tmp/make_qt.log &
 pid=$!
